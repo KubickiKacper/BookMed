07994694f7133422582fc9997cad3f21fb28ab56
Mona Lisa <ka46561@zut.edu.pl>
2022-03-15 17:45:22 +0100

    adding connection to database

--- app.py
+++ app.py
@@ -1,6 +1,26 @@
+import mariadb 
+import sys
 from flask import Flask,render_template
+
 app = Flask(__name__)
 
+try:
+    conn=mariadb.connect(
+        user='bookmed_admin',
+        password="bookmedpassword1215",
+        host="35.209.219.71",
+        port=3306,
+        database="bookmed"
+    )
+except mariadb.Error as e:
+    print(f"Error connecting to MariaDB Platform: {e}")
+    sys.exit(1)
+
+cur = conn.cursor()
+#cur.execute("SELECT * FROM doctors") 
+#tables = cur.fetchall()
+#for row in tables:
+#    print(row)
 
 @app.route('/')
 def homePage():
